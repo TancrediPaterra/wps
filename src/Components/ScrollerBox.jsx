1dabeda89abf865581d628beca4ea0c17111d5a2
Tancredi.Paterra <tancredi.paterra@gmail.com>
2024-10-01 18:29:08 +0200

    fix varie post stefano

--- src/Components/ScrollerBox.jsx
+++ src/Components/ScrollerBox.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Arrow from "./Arrow";
+import {ReactComponent as MenuIcon} from "../Assets/Menu.svg";
 
 function ScrollerBox({floor, setFloor, toggleMenu}) {
     return <div className={"scroller-box"}>
         <button className="open-menu-button" onClick={toggleMenu}>
-            <img src={"/Assets/ICON_MENU.png"} alt="Logo" style={{width: '60px', height: '30px'}}/>
+            <MenuIcon/>
         </button>
         <div className={"arrow-box"}>
             <Arrow floor={floor} setFloor={setFloor}/>
