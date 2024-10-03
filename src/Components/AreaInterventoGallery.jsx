67bc63087ca3f25c49ff2e1f00205049080ea984
Tancredi.Paterra <tancredi.paterra@gmail.com>
2024-10-03 17:19:55 +0200

    fix varie post stefano

--- src/Components/AreaInterventoGallery.jsx
+++ src/Components/AreaInterventoGallery.jsx
@@ -3,7 +3,7 @@ function AreaInterventoGallery({name}){
 
     return <div className={`area-intervento-gallery`}>
             <img src={"/Assets/BulletCube.png"} alt={"bullet-cube"}/>
-            <div className={`titolo-area-intervento`}>{name}•<b>GALLERY</b> </div>
+            <div className={`titolo-area-intervento`}>{name} • <b>GALLERY</b> </div>
     </div>
 }
 
