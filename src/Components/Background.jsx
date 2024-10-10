508f6a0e10e98a02f25d4af256d8ae7f4ebacf1f
Tancredi.Paterra <tancredi.paterra@gmail.com>
2024-10-10 12:58:03 +0200

    Gestione cambio background home

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useMemo, useRef} from "react";
 
 import BACKGROUND_0 from "../Assets/BACKGROUNDS/BACKGROUND_HOME_BLACK_CLEAR.webp";
 import BACKGROUND_1 from "../Assets/BACKGROUNDS/BACKGROUND_HOME_PRESENTAZIONE_CLEAR.webp";
@@ -19,17 +19,19 @@ export default function Background({actualFloor, direction }) {
     const backgrounds:string[] =[BACKGROUND_0,BACKGROUND_1, BACKGROUND_2, BACKGROUND_3, BACKGROUND_4, BACKGROUND_5];
     const backgroundsImages=useRef([]);
 
-    function test(){
+    function RenderSwiperSlides() {
         backgroundsImages.current = backgrounds.map((src) => {
             const img = new Image();
             img.src = src;
-            img.className=".swiper-slide-img"
+            img.className = ".swiper-slide-img"
             return img;
         });
-        return backgroundsImages.current.map((img, index) => (
-            <SwiperSlide key={index} className={"swiper-slide"}>
-                <img src={img.src} alt={`Background ${index}`} className="swiper-slide-img"/>
-            </SwiperSlide>))
+        return useMemo(() => {
+            return backgroundsImages.current.map((img, index) => (
+                <SwiperSlide key={index} className={"swiper-slide"}>
+                    <img src={img.src} alt={`Background ${index}`} className="swiper-slide-img"/>
+                </SwiperSlide>))
+        })
     }
 
     //Montaggio Ref Swiper
@@ -48,18 +50,13 @@ export default function Background({actualFloor, direction }) {
         onSwiper={onSwiper}
         modules={[EffectCube]}
         initialSlide={0}
-        cubeEffect={{shadow: false}}
+        cubeEffect={{shadow: false, slideShadows: false}}
         effect={"cube"}
         direction={'vertical'}
         className="swiper"
         enabled={"false"}
 
     >
-        {test()}
-        {/*{backgroundsImages && backgroundsImages.current.map((img, index) => (*/}
-        {/*    <SwiperSlide key={index} className={"swiper-slide"}>*/}
-        {/*        <img src={img.src} alt={`Background ${index}`} className="swiper-slide-img"/>*/}
-        {/*    </SwiperSlide>))*/}
-        {/*}*/}
+        {RenderSwiperSlides()}
     </Swiper>
 }
--- src/Macro-Components/Home.jsx
+++ src/Macro-Components/Home.jsx
@@ -8,9 +8,11 @@ import MenuLaterale from "../Components/MenuLaterale";
 import ScrollerBox from "../Components/ScrollerBox";
 import {ReactComponent as Logo} from "../Assets/SVG/Logo.svg";
 import Background from "../Components/Background";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 
 
 function Home({floors, isMenuOpen, toggleMenu}) {
+    const totalLevels = 5;
     const [state, setState] = useState({
         precedentFloor: 0,
         floor: 0,
@@ -29,15 +31,35 @@ function Home({floors, isMenuOpen, toggleMenu}) {
             })
         }
     }
-    const maxScroll = window.innerHeight/10; // Numero arbitrario che definisce il massimo dello scroll (puoi personalizzarlo)
-    const totalLevels = 5;  // Il numero di fasce di livello che desideri (6 in questo caso)
-    let scrollAmountRef = useRef(0); // Variabile per tenere traccia dello scroll simulato
-    const isThrottling = useRef(false);
 
+    //Gestione scroll
+    function upHandler(){
+        if (state.floor>0){
+            const precedentFloor= state.floor;
+            const floor = state.floor-1;
+            const direction = "up";
+            setState({
+                precedentFloor:precedentFloor,
+                floor: floor,
+                direction: direction
+            })
+        }
+    }
+    function downHandler(){
+        if (state.floor<totalLevels){
+            const precedentFloor= state.floor;
+            const floor = state.floor+1;
+            const direction = "down";
+            setState({
+                precedentFloor:precedentFloor,
+                floor: floor,
+                direction: direction
+            })
+        }
+    }
 
-
+    //Animazione frecce
     useEffect(() => {
-        // //animazione arrows
         if(!isMenuOpen){
             if(state.precedentFloor!==state.floor){
                 const arrowElement = document.querySelector(`.arrow-${state.direction}`);
@@ -50,56 +72,15 @@ function Home({floors, isMenuOpen, toggleMenu}) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [state]);
 
-    //scroll event handler
-    useEffect(() => {
-        const handleScroll = (event) => {
-            if (isThrottling.current) return;
-
-            // Aggiorna la variabile di scrollAmount in base all'evento della rotella o del tocco
-            let delta = Math.min(event.deltaY || event.touches?.[0].clientY || 0, maxScroll);
-            scrollAmountRef.current += delta;
-
-            // Limita scrollAmount tra 0 e maxScroll
-            scrollAmountRef.current = Math.max(0, Math.min(scrollAmountRef.current, maxScroll));
-
-
-            // Calcola il livello basato sullo scroll
-            const newFloor = Math.floor((scrollAmountRef.current / maxScroll) * totalLevels);
-
-            // Aggiorna lo stato solo se il livello cambia e blocca eventuali aggiornamenti fino alla fine del timeout
-            if (newFloor !== state.floor) {
-                changeState(newFloor);
-                isThrottling.current = true;
-                setTimeout(() => {
-                    isThrottling.current = false;
-                }, 1200);
-            }
-        }
-
-        // Aggiungi gli event listener per wheel e touchmove
-        window.addEventListener('wheel', handleScroll);
-        window.addEventListener('touchmove', handleScroll);
-
-        return () => {
-            // Rimuovi gli event listener quando il componente viene smontato
-            window.removeEventListener('wheel', handleScroll);
-            window.removeEventListener('touchmove', handleScroll);
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-
-    },         // eslint-disable-next-line react-hooks/exhaustive-deps
-        [state.floor]);
-
-
 
     return <div className={"home"}>
-        <Background actualFloor={state.floor} direction={state.direction}/>
-        <div className="logo-grande" onClick={()=>changeState(0)}>
-            <Logo className={"logo-grande-svg"}/>
-        </div>
-        <DiagonaleCubi floors={floors} actualFloor={state.floor} setFloor={changeState}/>
-        <DiagonaleAree floors={floors} actualFloor={state.floor} setFloor={changeState}/>
-        {isMenuOpen ? <MenuLaterale toggleMenu={toggleMenu}/> : <ScrollerBox floor={state.floor} setFloor={changeState} toggleMenu={toggleMenu}/>}
+        <ReactScrollWheelHandler upHandler={upHandler} downHandler={downHandler}>
+            <Background actualFloor={state.floor} direction={state.direction}/>
+            <Logo className={"logo-grande"} onClick={()=>changeState(0)}/>
+            <DiagonaleCubi floors={floors} actualFloor={state.floor} setFloor={changeState}/>
+            <DiagonaleAree floors={floors} actualFloor={state.floor} setFloor={changeState}/>
+            {isMenuOpen ? <MenuLaterale toggleMenu={toggleMenu}/> : <ScrollerBox floor={state.floor} setFloor={changeState} toggleMenu={toggleMenu}/>}
+        </ReactScrollWheelHandler>
 
     </div>
 }
